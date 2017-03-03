7df757c47b0de11333be3fad0d1e7e911612e0ca
Michael Schilling <mschilling@arkid.nl>
2017-03-03 20:37:21 +0100

    Add config + env configuration

---
+++ config/default.js
@@ -0,0 +1,8 @@
+'use strict';
+
+module.exports = {
+  firebase: {
+    databaseURL: process.env.FIREBASE_DB_URL,
+    serviceAccount: process.env.FIREBASE_SERVICE_ACCOUNT
+  }
+};
---
+++ config/index.js
@@ -0,0 +1,6 @@
+'use strict';
+
+const _ = require('lodash');
+const defaults = require('./default.js');
+const config = require('./' + (process.env.NODE_ENV || 'development') + '.js');
+module.exports = _.merge({}, defaults, config);
