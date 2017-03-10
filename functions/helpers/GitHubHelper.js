795b57e7163a91e32fe85de80ee21ef19e8c0b9f
Michael Schilling <mschilling@arkid.nl>
2017-03-10 16:39:12 +0100

    Update Github helper

--- functions/helpers/GitHubHelper.js
+++ functions/helpers/GitHubHelper.js
@@ -6,15 +6,22 @@ class GitHubHelper {
 
     const commits = [];
     payload.commits.forEach( (commit) => {
+      console.log(commit.author);
       commits.push({
         id: commit.id,
         // treeId: commit.tree_id,
         timestamp: commit.timestamp,
+        distinct: commit.distinct,
         repo: payload.repository.name,
         message: commit.message,
-        user: ( (commit.author && commit.author.username) ? commit.author.username :
-          ( (commit.committer & commit.committer.username) ? commit.committer.username :
-          'unknown' ) )
+        user: (
+          ( commit.author && commit.author.username ) ? commit.author.username : (
+              ( commit.committer & commit.committer.username ) ? commit.committer.username : (
+                ( commit.author & commit.author.name ) ? commit.committer.username :
+                'onbekend'
+              )
+            )
+          )
       });
     });
     return commits;
