d36687e64568fbdc4cf9743efaf78c62402807e1
Michael Schilling <mschilling@arkid.nl>
2017-03-22 22:41:20 +0100

    Functions code cleanup (Firebase)

--- functions/index.js
+++ functions/index.js
@@ -45,25 +45,3 @@ exports.onGitHubCommits = functions.database.ref('/raw/github/{pushId}')
       console.log('onGitHubPush', commits);
     }
   });
-// Listens for new messages added to /messages/:pushId/original and creates an
-// uppercase version of the message to /messages/:pushId/uppercase
-// exports.makeUppercase = functions.database.ref('/cloud_test/{pushId}/original')
-//   .onWrite(event => {
-//     // Grab the current value of what was written to the Realtime Database.
-//     const original = event.data.val();
-//     console.log('Uppercasing', event.params.pushId, original);
-//     const uppercase = original.toUpperCase();
-//     // You must return a Promise when performing asynchronous tasks inside a Functions such as
-//     // writing to the Firebase Realtime Database.
-//     // Setting an "uppercase" sibling in the Realtime Database returns a Promise.
-//     return event.data.ref.parent.child('uppercase').set(uppercase);
-//   });
-
-// exports.dummy = functions.database.ref('/raw/github/{pushId}')
-//   .onWrite(event => {
-//     const obj = event.data.val();
-//     const targetRef = event.data.ref.root.child(`test/michael/${event.params.pushId}`);
-//     console.log('processGitHubInput', event.params.pushId, obj);
-//     obj.timestamp = new Date().toISOString();
-//     return targetRef.set(obj);
-//   });
