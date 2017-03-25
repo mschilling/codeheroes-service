802fa3ea69dfffe44d49a9f356c38e4458785838
Michael Schilling <mschilling@arkid.nl>
2017-03-25 15:35:58 +0100

    Code cleanup

--- functions/helpers/GitHubHelper.js
+++ functions/helpers/GitHubHelper.js
@@ -21,35 +21,6 @@ class GitHubHelper {
     }
     return githubEventTypes.undefined;
   }
-  // static parseCommits(payload) {
-  //   if (!payload || !payload.commits) return [];
-
-  //   const commits = [];
-  //   payload.commits.forEach((commit) => {
-  //     const author = commit.author || {};
-  //     const committer = commit.committer || {};
-
-  //     const entry = {
-  //       id: commit.id,
-  //       // treeId: commit.tree_id,
-  //       timestamp: commit.timestamp,
-  //       distinct: commit.distinct,
-  //       repo: payload.repository.name,
-  //       message: commit.message,
-  //     };
-
-  //     if (author.name) entry.author_name = author.name;
-  //     if (author.username) entry.author_username = author.username;
-  //     if (author.email) entry.author_email = author.email;
-
-  //     if (committer.name) entry.committer_name = committer.name;
-  //     if (committer.username) entry.committer_username = committer.username;
-  //     if (committer.email) entry.committer_email = committer.email;
-
-  //     commits.push(entry);
-  //   });
-  //   return commits;
-  // }
 
   static parsePush(payload) {
     if (!payload || !payload.pusher) return;
@@ -62,6 +33,7 @@ class GitHubHelper {
     push.commits_count = (payload.commits || []).length;
     return push;
   }
+
   static parseRepoFromPayload(payload) {
     const { name, full_name: fullName, organization, owner } = payload.repository;
     const repo = {
