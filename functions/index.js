c989f85905b534a200fb78cdd9db9c615c9895c9
Michael Schilling <mschilling@arkid.nl>
2017-03-26 20:54:20 +0200

    Fix onPush handler in FB functions

--- functions/index.js
+++ functions/index.js
@@ -7,27 +7,27 @@ admin.initializeApp(functions.config().firebase);
 const GitHubPayload = require('./helpers/GitHubPayload');
 const Scores = require('./helpers/Scores');
 
-// const gh = require('./helpers/GitHubHelper');
+const gh = require('./helpers/GitHubHelper');
 const ghEventTypes = require('./constants/github_event_types');
 
-// exports.processGitHubInput = functions.database.ref('/raw/github/{pushId}')
-//   .onWrite(event => {
-//     const ref = event.data.adminRef;
-//     const data = event.data.val();
+exports.processGitHubInput = functions.database.ref('/raw/github/{pushId}')
+  .onWrite(event => {
+    const ref = event.data.adminRef;
+    const data = event.data.val();
 
-//     const commits = gh.parseCommitsFromPayload(data);
-//     return commits.forEach((commit) => {
-//       ref.root.child('/on/commit').push(commit);
-//     });
-//   });
+    const commits = gh.parseCommitsFromPayload(data);
+    return commits.forEach((commit) => {
+      ref.root.child('/on/commit').push(commit);
+    });
+  });
 
-// exports.processGitHubInputPushes = functions.database.ref('/raw/github/{pushId}')
-//   .onWrite(event => {
-//     const push = gh.parsePush(event.data.val());
-//     if (push) {
-//       event.data.ref.root.child('/on/push').push(push);
-//     }
-//   });
+exports.processGitHubInputPushes = functions.database.ref('/raw/github/{pushId}')
+  .onWrite(event => {
+    const push = gh.parsePush(event.data.val());
+    if (push) {
+      event.data.ref.root.child('/on/push').push(push);
+    }
+  });
 
 // exports.onGitHubPush = functions.database.ref('/raw/github/{pushId}')
 //   .onWrite(event => {
@@ -44,12 +44,13 @@ const ghEventTypes = require('./constants/github_event_types');
 
 exports.onGitHubPushEvent = functions.database.ref('/raw/github/{pushId}')
   .onWrite(event => {
-    const ref = event.data.adminRef;
+    const ref = event.data.adminRef.root;
     const data = event.data.val();
 
     const github = new GitHubPayload(data);
     if (github.eventType == ghEventTypes.push) {
       const score = new Scores(ref);
+        // const actions = github.commits.map( () => )
       return github.commits.forEach(c => {
         score.onCommit(c);
       });
@@ -57,3 +58,4 @@ exports.onGitHubPushEvent = functions.database.ref('/raw/github/{pushId}')
       Promise.resolve();
     }
   });
+
