85cecd97a69e9eeeef930582de337e4326cfda52
Michael Schilling <mschilling@arkid.nl>
2017-03-29 23:02:10 +0200

    Remove unused code

--- functions/index.js
+++ functions/index.js
@@ -15,24 +15,24 @@ const ghEventTypes = require('./constants/github_event_types');
 
 exports.authNewUser = functions.auth.user().onCreate(user.createUser);
 
-exports.processGitHubInput = functions.database.ref('/raw/github/{pushId}')
-  .onWrite(event => {
-    const ref = event.data.adminRef;
-    const data = event.data.val();
+// exports.processGitHubInput = functions.database.ref('/raw/github/{pushId}')
+//   .onWrite(event => {
+//     const ref = event.data.adminRef;
+//     const data = event.data.val();
 
-    const commits = gh.parseCommitsFromPayload(data);
-    return commits.forEach((commit) => {
-      ref.root.child('/on/commit').push(commit);
-    });
-  });
+//     const commits = gh.parseCommitsFromPayload(data);
+//     return commits.forEach((commit) => {
+//       ref.root.child('/on/commit').push(commit);
+//     });
+//   });
 
-exports.processGitHubInputPushes = functions.database.ref('/raw/github/{pushId}')
-  .onWrite(event => {
-    const push = gh.parsePush(event.data.val());
-    if (push) {
-      event.data.ref.root.child('/on/push').push(push);
-    }
-  });
+// exports.processGitHubInputPushes = functions.database.ref('/raw/github/{pushId}')
+//   .onWrite(event => {
+//     const push = gh.parsePush(event.data.val());
+//     if (push) {
+//       event.data.ref.root.child('/on/push').push(push);
+//     }
+//   });
 
 exports.onGitHubPushEvent = functions.database.ref('/raw/github/{pushId}')
   .onWrite(event => {
