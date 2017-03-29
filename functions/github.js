94941450c842ac1426006e6e593e8a1f9c516fb0
Michael Schilling <mschilling@arkid.nl>
2017-03-29 23:15:30 +0200

    Refactor Functions entrypoint (index.js) + major code cleanup :-)

---
+++ functions/github.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const GitHubPayload = require('./helpers/GitHubPayload');
+const Scores = require('./helpers/Scores');
+const fh = require('./helpers/FirebaseHelper');
+const eventTypes = require('./constants/github_event_types');
+
+function onGitHubPushEvent(evt) {
+  const ref = evt.data.adminRef.root;
+  const data = evt.data.val();
+  let appSettings;
+
+  const github = new GitHubPayload(data);
+  fh.initialize(ref);
+
+  fh.getAppSettings()
+    .then((settings) => appSettings = settings)
+    .then(() => {
+      if (appSettings.excludeRepos.indexOf(github.repository.fullName) > -1) {
+        return Promise.resolve();
+      }
+      if (github.eventType == eventTypes.push) {
+        const score = new Scores(ref);
+        return github.distinctCommits.forEach(c => {
+          score.onCommit(c);
+        });
+      } else {
+        Promise.resolve();
+      }
+    });
+}
+
+// exports.processGitHubInput = functions.database.ref('/raw/github/{pushId}')
+//   .onWrite(event => {
+//     const ref = event.data.adminRef;
+//     const data = event.data.val();
+
+//     const commits = gh.parseCommitsFromPayload(data);
+//     return commits.forEach((commit) => {
+//       ref.root.child('/on/commit').push(commit);
+//     });
+//   });
+
+// exports.processGitHubInputPushes = functions.database.ref('/raw/github/{pushId}')
+//   .onWrite(event => {
+//     const push = gh.parsePush(event.data.val());
+//     if (push) {
+//       event.data.ref.root.child('/on/push').push(push);
+//     }
+//   });
+
+module.exports = {
+  onGitHubPushEvent: onGitHubPushEvent
+};
--- functions/index.js
+++ functions/index.js
@@ -4,35 +4,17 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
-const user = require('./user');
 const github = require('./github');
+const user = require('./user');
 
-// const GitHubPayload = require('./helpers/GitHubPayload');
-// const Scores = require('./helpers/Scores');
-// const fh = require('./helpers/FirebaseHelper');
-// const gh = require('./helpers/GitHubHelper');
-// const ghEventTypes = require('./constants/github_event_types');
+// Firebase Auth handlers
+const authNewUser = functions.auth.user().onCreate(user.createUser);
 
-exports.authNewUser = functions.auth.user().onCreate(user.createUser);
-
-// exports.processGitHubInput = functions.database.ref('/raw/github/{pushId}')
-//   .onWrite(event => {
-//     const ref = event.data.adminRef;
-//     const data = event.data.val();
-
-//     const commits = gh.parseCommitsFromPayload(data);
-//     return commits.forEach((commit) => {
-//       ref.root.child('/on/commit').push(commit);
-//     });
-//   });
-
-// exports.processGitHubInputPushes = functions.database.ref('/raw/github/{pushId}')
-//   .onWrite(event => {
-//     const push = gh.parsePush(event.data.val());
-//     if (push) {
-//       event.data.ref.root.child('/on/push').push(push);
-//     }
-//   });
-
-exports.onGitHubPushEvent = functions.database.ref('/raw/github/{pushId}')
+// Firebase Database handlers
+const onGitHubPushEvent = functions.database.ref('/raw/github/{pushId}')
   .onWrite(github.onGitHubPushEvent);
+
+module.exports = {
+    authNewUser: authNewUser,
+    onGitHubPushEvent: onGitHubPushEvent
+};
