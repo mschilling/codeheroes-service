df07e25990996a79d18b914b750267d98068156c
Michael Schilling <mschilling@arkid.nl>
2017-04-24 21:29:15 +0200

    Add data paths to constant

--- functions/constants/firebase_paths.js
+++ functions/constants/firebase_paths.js
@@ -1,10 +1,16 @@
 'use strict';
 
- const FIREBASE_PATHS = {
-  githubQueue: 'queues/github-hooks',
-  jiraQueue: 'queues/jira-hooks',
-  jenkinsQueue: 'queues/jenkins-hooks',
-  travisQueue: 'queues/travis-hooks'
+const queueRoot = 'queues';
+const dataRoot = 'data';
+
+const FIREBASE_PATHS = {
+  dataRoot: dataRoot,
+  feedData: `${dataRoot}/feed`,
+  queueRoot: queueRoot,
+  githubQueue: `${queueRoot}/github-hooks`,
+  jiraQueue: `${queueRoot}/jira-hook`,
+  jenkinsQueue: `${queueRoot}/jenkins-hooks`,
+  travisQueue: `${queueRoot}/travis-hooks`
 };
 
 module.exports = FIREBASE_PATHS;
