e688494121c10dac2cc9c20fa39da9774b033b0a
Michael Schilling <mschilling@arkid.nl>
2017-04-24 22:25:07 +0200

    Fix: Copy object properties using Object.assing

--- functions/queue.js
+++ functions/queue.js
@@ -43,7 +43,7 @@ function processHookFromQueue(evt) {
 
   return ref.child('raw').child(source).child(evt.data.key).once('value', function(snapshot) {
     const data = snapshot.val();
-    data._meta = Object.assign({}, data);
+    data._meta = Object.assign({}, eventData);
     return ref.child(paths.feedData).child(snapshot.key).set(data);
   }).then(() => evt.data.ref.remove())
   ;
