a09aa06d1709d5b5c02633e5bd188b29a070ce91
Michael Schilling <mschilling@arkid.nl>
2017-05-12 16:57:15 +0200

    Improve user onCreate handler (firebase function)

--- functions/user.js
+++ functions/user.js
@@ -6,16 +6,15 @@ function createUser(evt) {
     const user = evt.data; // The Firebase user. Type: functions.auth.UserRecord
 
     const email = user.email;
-    const displayName = user.displayName;
-
     const userObj = {
-        name: displayName,
         email: email,
-        // notificationEnabled: false,
-        // notificationTokens: {},
         profile_picture: user.photoURL,
         // providerData: user.providerData
     };
+
+    if(user.displayName) {
+      userObj.name = user.displayName;
+    }
 
     admin.database().ref('users/'+user.uid).update(userObj);
 }
