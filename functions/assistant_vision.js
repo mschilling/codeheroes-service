455a74d579c3d50370d10172d42c1049bdd39bd0
Michael Schilling <mschilling@arkid.nl>
2017-07-20 14:21:42 +0200

    Fix Assistant/Vision database reference

--- functions/assistant_vision.js
+++ functions/assistant_vision.js
@@ -1,9 +1,14 @@
 'use strict';
 
+const moment = require('moment');
+const admin = require('firebase-admin');
+const ref = admin.database().ref();
+
 const ApiAiApp = require('actions-on-google').ApiAiApp;
 
 const ACTIONS = {
-  'tell.joke': {callback: tellJokeHandler}
+  'tell.joke': {callback: tellJokeHandler},
+  'tell.commit.info': {callback: tellCommitInfoHandler}
 };
 
 function webhook(request, response) {
@@ -13,31 +18,33 @@ function webhook(request, response) {
 }
 
 // eslint-disable-next-line no-unused-vars
-function getRandomQuote(options) {
-   firebase.database()
-           .ref('quotes')
-           .once('value')
-           .then(function(snapshot) {
-              const quotes = snapshot.val() || [];
-              const randomQuote = quotes[Math.floor(Math.random()*quotes.length)];
-              options.onSuccess(randomQuote);
-           });
-}
-
 function tellJokeHandler(app) {
   console.log('Came to Tell Joke');
   // const fromFallback = app.getArgument('fromFallback');
   // const timeQuery = app.getArgument('time-query');
-  let message;
-
-  getGroupInfo({
-      onSuccess: function(quote) {
-        message = quote;
-        console.log(message);
-        app.ask(message);
-      }
+  ref.child('quotes')
+    .once('value')
+    .then(function(snapshot) {
+      const quotes = snapshot.val() || [];
+      const randomQuote = quotes[Math.floor(Math.random()*quotes.length)];
+      app.ask(randomQuote);
     });
-  app.ask('Testing, one two three');
+}
+
+function tellCommitInfoHandler(app) {
+  const timestamp = moment();
+  const dayKey = timestamp.format('YYYYMMDD');
+
+  ref.child(`/metrics/user/commits_per_day/${dayKey}`)
+    .limitToLast(1)
+    .once('value')
+    .then(function(snapshot) {
+      snapshot.forEach(function(childSnapshot) {
+        const commit = childSnapshot.val();
+        const message = `The last commit is by ${commit.name}. Here's the commit message: ${commit.message}`;
+        app.ask(message);
+      });
+    });
 }
 
 module.exports = {
