f2a15df37f7912143580956b8233b79a69d28964
Michael Schilling <mschilling@arkid.nl>
2017-11-27 08:39:49 +0100

    Update weekly leaderboard key

--- leaderboard_service.js
+++ leaderboard_service.js
@@ -21,7 +21,7 @@ const lb = new Leaderboard();
 let currentKey;
 let users = [];
 
-const weekId = '-KzL6oT8NXXmwnu0akhr_2';
+const weekId = '-KztzAnwlI3qtLXYGIBg_2';
 
 const query = ref.child('raw/github')
   .orderByKey()
